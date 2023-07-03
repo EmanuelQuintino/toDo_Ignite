0d20a8867d8511f6630975dbd71c69a80d85b130
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-07-03 14:42:53 -0300

    add box totals to do

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,22 @@
 import { Text, View } from 'react-native';
 import { LogoHome } from "../../components/LogoHome";
+import { styles } from './style';
 
 export function Home() {
   return (
-    <View>
+    <View style={styles.container}>
       <LogoHome />
-      <Text>Hello World!</Text>
+      <View style={styles.box}>
+        <View style={styles.boxTotalToDos}>
+          <Text style={styles.created}>Criadas</Text>
+          <Text style={styles.total}>2</Text>
+        </View>
+
+        <View style={styles.boxTotalToDos}>
+          <Text style={styles.completed}>Concluídas</Text>
+          <Text style={styles.total}>3</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/Home/style.ts
+++ src/screens/Home/style.ts
@@ -2,8 +2,44 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#1f1e25",
+    flex: 1,
+    backgroundColor: "#1e1e1e",
     width: "100%",
-    padding: 16,
+  },
+
+  box: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 56,
+    padding: 24,
+  },
+
+  boxTotalToDos: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 8,
+  },
+
+  created: {
+    color: "#4ea8de",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  completed: {
+    color: "#8284fa",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  total: {
+    color: "#d9d9d9",
+    fontWeight: "bold",
+    fontSize: 16,
+    borderRadius: 100,
+    paddingHorizontal: 12,
+    backgroundColor: "#333333",
   },
 });
